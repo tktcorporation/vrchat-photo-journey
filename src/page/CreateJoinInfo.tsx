7d1f4efec6bcf8b47e5bd1dcaef2cda2fd150d53
tkt <37575408+tktcorporation@users.noreply.github.com>
2023-10-30 16:18:44 +0000

    refactor: 型アップデート

--- src/page/CreateJoinInfo.tsx
+++ src/page/CreateJoinInfo.tsx
@@ -7,20 +7,14 @@ function CreateJoinInfo() {
     'ready' | 'logFilesDirNotSet' | 'logFilesNotFound' | 'logFileDirNotFound' | null
   >(null);
   const [statusToUseVRChatPhotoDir, setStatusToUseVRChatPhotoDir] = React.useState<
-    'ready' | 'logFilesDirNotSet' | 'logFilesNotFound' | 'logFileDirNotFound' | null
+    'ready' | 'photoYearMonthDirsNotFound' | 'photoDirReadError' | null
   >(null);
   useEffect(() => {
     window.MyOn.receiveStatusToUseVRChatLogFilesDir((status) => {
       setStatusToUseVRChatLogFilesDir(status);
     });
-    window.Main.on('vrchat-photo-dir-with-error', ({ storedPath, error }) => {
-      let status: 'ready' | 'logFilesDirNotSet' | 'logFilesNotFound' = 'ready';
-      if (storedPath === null) {
-        status = 'logFilesDirNotSet';
-      } else if (error) {
-        status = 'logFilesNotFound';
-      }
-      setStatusToUseVRChatPhotoDir(status);
+    window.MyOn.receiveVRChatPhotoDirWithError((data) => {
+      setStatusToUseVRChatPhotoDir(data.error || 'ready');
     });
   }, []);
 
