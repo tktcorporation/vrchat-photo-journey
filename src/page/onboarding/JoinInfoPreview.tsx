b21d595555fff1a3ba9b7655747d2c7ec1980990
tkt <37575408+tktcorporation@users.noreply.github.com>
2023-11-29 17:40:02 +0000

    style: プレビューの回り込みとスクロール

--- src/page/onboarding/JoinInfoPreview.tsx
+++ src/page/onboarding/JoinInfoPreview.tsx
@@ -10,30 +10,32 @@ export function JoinInfoPreview({ className }: Props) {
   const infoMap = trpcReact.getWorldJoinInfoWithPhotoPath.useQuery().data;
 
   return (
-    <div className={cn('space-y-4 overflow-hidden', className)}>
+    <div className={cn('space-y-4 flex flex-col h-full', className)}>
       <div>
-        <p>このように、写真とワールドの情報を紐付けることができます</p>
+        <p>写真とワールドの情報を紐付けることができます</p>
         <p>
           (紐づけを行えるのは最後のVRChat起動から約24時間ほど前までに撮った写真です)
         </p>
       </div>
-      <div className="py-4 space-y-4 overflow-hidden">
-        <ScrollArea>
+      <ScrollArea>
+        <div className="flex-grow overflow-y space-y-8">
           {infoMap?.map((item) => {
             if (item.tookPhotoList.length === 0) return null;
             return (
-              <div>
-                <p className="text-lg">{item.world.worldName}</p>
-                <p className="text-sm text-gray-500">
-                  Join日時: {item.world.joinDatetime}
-                </p>
-                <div className="flex flex-row space-x-2">
+              <div className="space-y-3" key={item.world.joinDatetime}>
+                <div>
+                  <p className="text-lg">{item.world.worldName}</p>
+                  <p className="text-sm text-gray-500">
+                    Join日時: {item.world.joinDatetime}
+                  </p>
+                </div>
+                <div className="flex flex-wrap gap-4">
                   {item.tookPhotoList.map((photo) => {
                     return (
                       <Photo
                         photoPath={photo.photoPath}
                         className="w-32"
-                        key={photo.photoPath}
+                        key={photo.tookDatetime}
                       />
                     );
                   })}
@@ -41,8 +43,8 @@ export function JoinInfoPreview({ className }: Props) {
               </div>
             );
           })}
-        </ScrollArea>
-      </div>
+        </div>
+      </ScrollArea>
     </div>
   );
 }
