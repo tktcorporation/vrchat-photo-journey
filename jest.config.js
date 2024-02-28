46c3486ae8f7fe570fb1e662d2cb2655363ec720
tkt <37575408+tktcorporation@users.noreply.github.com>
2024-02-28 12:38:25 +0000

    fix: fix typo [playerite -> playwright]

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,12 @@ module.exports = {
   transform: {
     '^.+\\.(t|j)sx?$': '@swc/jest',
   },
-  testPathIgnorePatterns: ['/node_modules/', '/dist/', '/main/', '/playwrite/'],
+  testPathIgnorePatterns: [
+    '/node_modules/',
+    '/dist/',
+    '/main/',
+    '/playwright/',
+  ],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
