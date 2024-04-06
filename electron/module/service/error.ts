b4b15614435ca818a9601c0b52e21bc27ac87ff2
tkt <37575408+tktcorporation@users.noreply.github.com>
2024-04-06 17:15:20 +0000

    clean: 使わなさそうなファイルの削除

--- electron/module/service/error.ts
+++
@@ -1,6 +0,0 @@
-export class YearMonthPathNotFoundError extends Error {
-  constructor(yearMonthDir: string) {
-    super(`YearMonthPathNotFoundError: ${yearMonthDir} is not found`);
-    Error.captureStackTrace(this, YearMonthPathNotFoundError);
-  }
-}
