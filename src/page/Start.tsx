857d6ea198b38edcf6f63418f90b0fe0d8ba4151
tkt <37575408+tktcorporation@users.noreply.github.com>
2024-04-27 12:48:44 +0000

    feat: resetdb に確認ボタンを付ける(いらないかも)

--- src/page/Start.tsx
+++ src/page/Start.tsx
@@ -2,6 +2,8 @@ import { trpcReact } from '@/trpc';
 
 import { useNavigate } from 'react-router-dom';
 
+import { Button } from '@/components/ui/button';
+import * as card from '@/components/ui/card';
 import { ROUTER_PATHS } from '@/constants';
 import { Loader } from 'lucide-react';
 import { useEffect } from 'react';
@@ -15,14 +17,29 @@ export const Start = () => {
   });
 
   // TODO: 初回のみ実行するようにbackendで制御しておく？
-  useEffect(() => {
-    resetRdbMutation.mutate();
-  }, []);
+  useEffect(() => {}, []);
 
   return (
     <div className="flex-auto h-full flex flex-col space-y-9">
       <div className="w-3/5 mx-auto mt-6">
-        <Loader />
+        {resetRdbMutation.isLoading ? (
+          <div className="flex items-center justify-center h-64">
+            <Loader className="w-8 h-8" />
+          </div>
+        ) : (
+          <card.Card>
+            <card.CardHeader>
+              <card.CardTitle>sync db</card.CardTitle>
+            </card.CardHeader>
+            <card.CardContent>
+              データ構成に変更が検出されました。
+              データを初期化して同期を行います。
+            </card.CardContent>
+            <card.CardFooter>
+              <Button onClick={() => resetRdbMutation.mutate()}>開始</Button>
+            </card.CardFooter>
+          </card.Card>
+        )}
       </div>
     </div>
   );
