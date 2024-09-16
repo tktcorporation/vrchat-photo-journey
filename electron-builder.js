579bede2f82b0929b34fb7f9f3a127373f7a8627
tkt <37575408+tktcorporation@users.noreply.github.com>
2024-09-16 02:57:49 +0000

    build: release ビルドの方法を変更

---
+++ electron-builder.js
@@ -0,0 +1,34 @@
+/**
+ * @type {import('electron-builder').Configuration}
+ * @see https://www.electron.build/configuration/configuration
+ */
+const config = {
+  asar: true,
+  files: ['main', 'src/out'],
+  directories: {
+    buildResources: 'resources',
+  },
+  extraResources: [
+    {
+      from: './assets/',
+      to: 'assets',
+    },
+  ],
+  publish: [
+    {
+      provider: 'github',
+      owner: 'tktcorporation',
+      repo: 'vrchat-photo-journey',
+      releaseType:
+        process.env.NOT_DRAFT_RELEASE === 'true' ? 'release' : 'draft',
+    },
+  ],
+  win: {
+    target: 'nsis',
+  },
+  linux: {
+    target: 'AppImage',
+  },
+};
+
+module.exports = config;
