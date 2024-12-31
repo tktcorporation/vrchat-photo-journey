35920956eadb809c9e9dfcd520d68eae668252b8
tktcorporation <tktcorporation.go@gmail.com>
2024-12-31 17:29:11 +0000

    WorldJoinの単位でグルーピングするように

--- src/v2/components/PhotoGallery/useGroupPhotos.ts
+++ src/v2/components/PhotoGallery/useGroupPhotos.ts
@@ -1,40 +1,55 @@
+import { trpcReact } from '@/trpc';
 import { useMemo } from 'react';
-import { locationDetails } from '../../data/locationDetails';
 import type { Photo } from '../../types/photo';
 
-export interface GroupedPhotos {
-  [key: string]: {
-    photos: Photo[];
-    location: string;
-    date: string;
-    locationDetail: (typeof locationDetails)[keyof typeof locationDetails];
-  };
+export interface GroupedPhoto {
+  photos: Photo[];
+  worldId: string;
+  worldName: string;
+  worldInstanceId: string;
+  joinDateTime: Date;
 }
 
+export type GroupedPhotos = Record<string, GroupedPhoto>;
+
 export function useGroupPhotos(photos: Photo[]) {
+  const { data: joinLogs } =
+    trpcReact.vrchatWorldJoinLog.getVRChatWorldJoinLogList.useQuery({
+      orderByJoinDateTime: 'desc',
+    });
+
   return useMemo(() => {
+    if (!joinLogs) return {} as GroupedPhotos;
+
     // 日付でソート（新しい順）
     const sorted = [...photos].sort(
       (a, b) => b.takenAt.getTime() - a.takenAt.getTime(),
     );
 
-    return sorted.reduce((groups: GroupedPhotos, photo) => {
-      const date = photo.takenAt;
-      const dateKey = `${date.getFullYear()}年${date.getMonth() + 1}月`;
-      const locationKey = photo.location.name;
-      const groupKey = `${locationKey}/${dateKey}`;
+    return sorted.reduce<GroupedPhotos>((groups, photo) => {
+      // 写真の撮影時刻以前の最新のjoinLogを探す
+      const joinLog = joinLogs.find(
+        (log) => log.joinDateTime.getTime() <= photo.takenAt.getTime(),
+      );
+
+      if (!joinLog) return groups;
+
+      const groupKey = `${
+        joinLog.worldInstanceId
+      }/${joinLog.joinDateTime.getTime()}`;
 
       if (!groups[groupKey]) {
         groups[groupKey] = {
           photos: [],
-          location: locationKey,
-          date: dateKey,
-          locationDetail: locationDetails[locationKey],
+          worldId: joinLog.worldId,
+          worldName: joinLog.worldName,
+          worldInstanceId: joinLog.worldInstanceId,
+          joinDateTime: joinLog.joinDateTime,
         };
       }
 
       groups[groupKey].photos.push(photo);
       return groups;
     }, {});
-  }, [photos]);
+  }, [photos, joinLogs]);
 }
