6decbe3269855f0a167090ee59a4f3db4e903211
tktcorporation <tktcorporation.go@gmail.com>
2025-01-02 15:13:38 +0000

    sentryの導入

--- src/v2/main.tsx
+++ src/v2/main.tsx
@@ -1,3 +1,4 @@
+import { init } from '@sentry/electron/renderer';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
@@ -7,6 +8,12 @@ const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error('Failed to find root element');
 }
+
+// Sentryの初期化
+init({
+  dsn: 'https://0c062396cbe896482888204f42f947ec@o4504163555213312.ingest.us.sentry.io/4508574659837952',
+  debug: process.env.NODE_ENV === 'development',
+});
 
 createRoot(rootElement).render(
   <StrictMode>
