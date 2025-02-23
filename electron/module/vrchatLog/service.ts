2426c364e8983416dc1dd78a8a4722682e029677
tktcorporation <tktcorporation.go@gmail.com>
2025-02-23 18:08:13 +0000

    chore: bump version to 0.10.2 and update log parsing regex

--- electron/module/vrchatLog/service.ts
+++ electron/module/vrchatLog/service.ts
@@ -281,8 +281,9 @@ const extractPlayerLeaveInfoFromLog = (
 ): VRChatPlayerLeaveLog => {
   // 下記のように、プレイヤー名は空白を含む場合がある
   // 2025.01.08 00:22:04 Log        -  [Behaviour] OnPlayerLeft プレイヤー ⁄ A (usr_34a27988-a7e4-4d5e-a49a-ae5975422779)
+  // 2025.02.22 21:14:48 Debug      -  [Behaviour] OnPlayerLeft tkt (usr_3ba2a992-724c-4463-bc75-7e9f6674e8e0)
   const regex =
-    /(\d{4}\.\d{2}\.\d{2})\s+(\d{2}:\d{2}:\d{2})\s+Log\s+-\s+\[Behaviour\] OnPlayerLeft (.+?)(?:\s+\((usr_[^)]+)\))?$/;
+    /(\d{4}\.\d{2}\.\d{2})\s+(\d{2}:\d{2}:\d{2})\s+\S+\s+-\s+\[Behaviour\] OnPlayerLeft (.+?)(?:\s+\((usr_[^)]+)\))?$/;
   const matches = logLine.value.match(regex);
   if (!matches) {
     throw new Error(
