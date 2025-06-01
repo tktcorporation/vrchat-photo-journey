5bd712f7798e0c6b86151d2cbc60b9223f4ef1ae
tktcorporation <tktcorporation.go@gmail.com>
2025-06-01 13:37:25 +0000

    feat: スクリーンショット機能を強化し、タイムアウト処理を追加
    - スクリーンショットを撮影するためのヘルパー関数`screenshot`を追加し、ログ出力を改善しました。
    - 各画面でのスクリーンショットを撮影するテストをリファクタリングし、タイムアウト処理を実装しました。
    - 不要なプレビュー画像ファイルを削除しました。

--- playwright/example.spec.ts
+++ playwright/example.spec.ts
@@ -1,6 +1,7 @@
 import * as path from 'node:path';
 import { fileURLToPath } from 'node:url';
-import { _electron, test } from '@playwright/test';
+import { type Page, _electron, test } from '@playwright/test';
+import consola from 'consola';
 
 // ESモジュール環境で__dirnameの代わりに使用
 const __filename = fileURLToPath(import.meta.url);
@@ -15,27 +16,91 @@ const launchElectronApp = async () => {
   return electronApp;
 };
 
-test('各画面でスクショ', async () => {
-  // Launch Electron app.
-  const electronApp = await launchElectronApp();
+const screenshotPath = (title: string, suffix: string) => {
+  return path.join(__dirname, './previews', `${title}-${suffix}.png`);
+};
 
-  // Get the first window that the app opens, wait if necessary.
-  const page = await electronApp.firstWindow();
+const screenshot = async (page: Page, title: string, suffix: string) => {
+  await page.screenshot({ path: screenshotPath(title, suffix) });
+  const now = new Date().toISOString().split('T')[1].split('.')[0];
+  consola.log(`[${now}]: screenshot: ${screenshotPath(title, suffix)}`);
+};
 
-  // Print the title.
-  console.log(await page.title());
+const TIMEOUT = 10000;
 
-  // await page.evaluate((routerPath) => {
-  //   window.history.pushState({}, '', routerPath);
-  //   window.location.href = `#${routerPath}`;
-  // }, routerPath);
-  const screenshotPath = path.join(
-    __dirname,
-    './previews',
-    `${page.title()}.png`,
-  );
-  await page.screenshot({ path: screenshotPath });
+test(
+  '各画面でスクショ',
+  async () => {
+    // Launch Electron app.
+    const electronApp = await launchElectronApp();
 
-  // Exit app.
-  await electronApp.close();
-});
+    // Get the first window that the app opens, wait if necessary.
+    const page = await electronApp.firstWindow();
+
+    const title = await page.title();
+
+    // Print the title.
+    console.log(title);
+
+    // await page.evaluate((routerPath) => {
+    //   window.history.pushState({}, '', routerPath);
+    //   window.location.href = `#${routerPath}`;
+    // }, routerPath);
+
+    // 4s以内に処理が終わらなければエラー
+    const timeoutDecreaseTwo = TIMEOUT - 2000;
+    Promise.race([
+      new Promise((resolve) => setTimeout(resolve, timeoutDecreaseTwo)),
+      new Promise((_resolve) => {
+        setTimeout(async () => {
+          await screenshot(page, title, 'timeout');
+          throw new Error('Timeout');
+        }, timeoutDecreaseTwo);
+      }),
+    ]);
+
+    await screenshot(page, title, 'initial');
+
+    // 「同意する」が表示されればクリック、表示されなければ次へ進む
+    await page.waitForTimeout(500);
+    const isTermsButtonVisible = await page.isVisible('text=同意する');
+    if (isTermsButtonVisible) {
+      await screenshot(page, title, 'terms');
+      await page.click('text=同意する');
+    } else {
+      consola.log('「同意する」ボタンが表示されていません');
+    }
+
+    await page.waitForSelector('text=初期セットアップ');
+    await screenshot(page, title, 'setup');
+
+    // aria-label="ログファイルを選択" のボタンをクリック
+    const logFileInput = await page.waitForSelector(
+      '[aria-label="input-VRChatログファイルディレクトリ"]',
+    );
+    await logFileInput.click();
+    // まず全部消す
+    await page.keyboard.press('Control+A');
+    await page.keyboard.press('Delete');
+    // パスを入力
+    await page.keyboard.type(path.join(__dirname, '../debug/logs'));
+    const submitButton = await page.waitForSelector(
+      '[aria-label="送信-VRChatログファイルディレクトリ"]',
+    );
+    await submitButton.click();
+
+    const 設定を確認して続けるButton = await page.waitForSelector(
+      'text=設定を確認して続ける',
+    );
+    await 設定を確認して続けるButton.click();
+
+    // 最後の状態をスクショ
+    await screenshot(page, title, 'finalized');
+
+    // Exit app.
+    await electronApp.close();
+  },
+  {
+    timeout: TIMEOUT,
+  },
+);
